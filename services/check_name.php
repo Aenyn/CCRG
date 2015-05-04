260584981fed2668fd9144d0d1b1a587038066e1
Aenyn <william@panicot.fr>
2015-05-04 17:33:22 +0200

    Restricted dice revealer in usernames

--- services/check_name.php
+++ services/check_name.php
@@ -1,3 +1,3 @@
-<?php if(preg_match('/\w/',$firstLetter)&&(strpos(strtoupper($writer),'SYSTEM ANN')===false)&&(strpos(strtoupper($writer),'DICE ROLLER')===false)&&(strpos(strtoupper($writer),'KRAKO MANAGER')===false)) {
+<?php if(preg_match('/\w/',$firstLetter)&&(strpos(strtoupper($writer),'SYSTEM ANN')===false)&&(strpos(strtoupper($writer),'DICE ROLLER')===false)&&(strpos(strtoupper($writer),'KRAKO MANAGER')===false)&&(strpos(strtoupper($writer),'DICE REVEAL')===false)) {
 	$send = true;
 } ?>
