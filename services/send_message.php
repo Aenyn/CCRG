7c345324411f76b8d83fb5ef7108f265fc8d54c3
Aenyn <william@panicot.fr>
2015-05-12 11:47:09 +0200

    Autolink on messages starting with http://

--- services/send_message.php
+++ services/send_message.php
@@ -36,7 +36,7 @@
 			$request = $bdd->prepare($query);
 			
 			if(substr($content,0,7)==="http://") {
-				$content = "<a href='" . $content ."'/>"
+				$content = "<a href='" . $content ."'>". $content . "</a>";
 			}
 			
 			$request->execute(array(':content' => $content, ':writer' => $writer, ':ip' => $_SERVER['REMOTE_ADDR']));
